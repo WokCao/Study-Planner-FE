69457dc426e260146ea127e505a9c0ba338e2d12
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-11-30 02:18:15 +0700

    design login UI

--- src/components/FormLayout.tsx
+++ src/components/FormLayout.tsx
@@ -3,13 +3,12 @@ import Props from '../interface/Props';
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="bg-black text-white flex min-h-screen flex-col items-center pt-16 sm:justify-center sm:pt-0">
-      <div className="relative mt-12 w-full max-w-lg sm:mt-10">
-        <div className="relative -mb-px h-px w-full bg-gradient-to-r from-transparent via-sky-300 to-transparent"></div>
-        <div className="mx-5 border dark:border-b-white/50 dark:border-t-white/50 border-b-white/20 sm:border-t-white/20 rounded-lg border-white/20 border-l-white/20 border-r-white/20 sm:shadow-sm lg:rounded-xl lg:shadow-none">
-          {children}
-        </div>
+    <div className='bg-violet-100 text-violet-900 flex min-h-screen flex-col items-center pt-16 sm:justify-center sm:pt-0'>
+      <div className='flex justify-center items-center absolute top-5'>
+        <img src='/gem.ico'/>
+        <h1 className='font-bold text-xl ml-3'>StudyGem</h1>
       </div>
+      {children}
     </div>
   );
 };
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -50,15 +50,15 @@ function Login() {
 
   return (
 		<>
-			<div className="flex flex-col p-6">
+			<div className="flex flex-col justify-center items-center p-6">
 				<h3 className="text-xl font-semibold leading-6 tracking-tighter">Login</h3>
-				<p className="mt-1.5 text-sm font-medium text-white/50">Welcome back, enter your credentials to continue.</p>
+				<p className="mt-1.5 text-sm font-medium">Welcome back, enter your credentials to continue.</p>
 			</div>
 			<div className="p-6 pt-0">
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<div className="group relative rounded-lg border focus-within:border-sky-200 px-3 pb-1.5 pt-2.5 duration-200 focus-within:ring focus-within:ring-sky-300/30">
+				<form className="flex flex-col justify-center items-center" onSubmit={handleSubmit(onSubmit)}>
+					<div className="group relative border-b border-violet-900 w-80 px-3 pb-1.5 pt-2.5 duration-200">
 						<div className="flex justify-between">
-							<label className="text-xs font-medium text-muted-foreground group-focus-within:text-white text-gray-400">Username</label>
+							<label className="text-xs font-medium text-muted-foreground">Username</label>
 						</div>
 						<input type="text" autoComplete="off" {...register('username', { required: 'Username is required' })}
 							className="block w-full border-0 bg-transparent p-0 text-sm file:my-1 file:rounded-full file:border-0 file:bg-accent file:px-4 file:py-2 file:font-medium placeholder:text-muted-foreground/90 focus:outline-none focus:ring-0 sm:leading-7 text-foreground" />
@@ -66,9 +66,9 @@ function Login() {
 					{errors.username ? <p className="pt-2 text-red-600">{errors.username.message}</p>
 					: errorUsername && <p className="pt-2 text-red-600">{errorUsername}</p>}
 
-					<div className="mt-4 group relative rounded-lg border focus-within:border-sky-200 px-3 pb-1.5 pt-2.5 duration-200 focus-within:ring focus-within:ring-sky-300/30">
+					<div className="mt-4 group relative border-b border-violet-900 w-80 px-3 pb-1.5 pt-2.5 duration-200">
 						<div className="flex justify-between">
-							<label className="text-xs font-medium text-muted-foreground group-focus-within:text-white text-gray-400">Password</label>
+							<label className="text-xs font-medium text-muted-foreground">Password</label>
 						</div>
 						<input type="password" {...register('password', { required: 'Password is required' })}
 							className="block w-full border-0 bg-transparent p-0 text-sm file:my-1 placeholder:text-muted-foreground/90 focus:outline-none focus:ring-0 focus:ring-teal-500 sm:leading-7 text-foreground" />
@@ -76,17 +76,20 @@ function Login() {
 					{errors.password ? <p className="pt-2 text-red-600">{errors.password.message}</p>
 					: errorPassword && <p className="pt-2 text-red-600">{errorPassword}</p>}
 
-					<div className="mt-4 flex items-center justify-end gap-x-2">
+					<div className="mt-5 flex flex-col justify-center items-center gap-x-2">
 						{fetching
 						? <ClipLoader size={30} color={"white"} loading={true} />
-						: <>
-								<a className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:ring hover:ring-white h-10 px-4 py-2 duration-200"
-									href="./register">Register</a>
-								<button
-									className="font-semibold hover:bg-black hover:text-white hover:ring hover:ring-white transition duration-300 inline-flex items-center justify-center rounded-md text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-white text-black h-10 px-4 py-2"
-									type="submit">Log in</button>
-							</>
+						: <button
+								className="bg-gradient-to-r from-violet-800 to-violet-500 text-white rounded-xl px-10 py-2 text-sm font-semibold inline-flex items-center justify-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+								type="submit">Log in</button>
 						}
+					</div>
+
+					<div className="mt-16 w-2/3 flex flex-col justify-center items-center gap-x-2">
+						<a className="border border-violet-900/50 rounded-xl w-full py-1 text-sm font-medium flex items-center justify-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+							href="./register">Sign up with Email</a>
+						<a className="border border-violet-900/50 rounded-xl w-full mt-5 py-1 text-sm font-medium flex items-center justify-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+							href="./register">Sign up with Google</a>
 					</div>
 				</form>
 			</div>
