3d6988c695b9adcfd8ea43030e04ff23a6cf6ee6
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-11-30 02:59:20 +0700

    extract button and anchor

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,6 +5,8 @@ import { ClipLoader } from 'react-spinners';
 
 import UserLogin from '../interface/UserLogin';
 import FormInput from './elements/FormInput';
+import ButtonPrimary from './elements/ButtonPrimary';
+import Anchor from './elements/Anchor';
 
 const API = import.meta.env.DEV ? import.meta.env.VITE_REACT_APP_API_LOCAL : import.meta.env.VITE_REACT_APP_API;
 
@@ -72,21 +74,15 @@ function Login() {
 						errors={errors}
 						error={errorPassword}
 					/>
-
 					<div className="mt-5 flex flex-col justify-center items-center gap-x-2">
 						{fetching
 						? <ClipLoader size={30} color={"white"} loading={true} />
-						: <button
-								className="bg-gradient-to-r from-violet-800 to-violet-500 text-white rounded-xl px-10 py-2 text-sm font-semibold inline-flex items-center justify-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-								type="submit">Log in</button>
+						: <ButtonPrimary label="Log in" type="submit" />
 						}
 					</div>
-
 					<div className="mt-16 w-2/3 flex flex-col justify-center items-center gap-x-2">
-						<a className="border border-violet-900/50 rounded-xl w-full py-1 text-sm font-medium flex items-center justify-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-							href="./register">Sign up with Email</a>
-						<a className="border border-violet-900/50 rounded-xl w-full mt-5 py-1 text-sm font-medium flex items-center justify-center focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-							href="./register">Sign up with Google</a>
+						<Anchor label="Sign up with Email" href="./register" />
+						<Anchor label="Sign up with Google" href="./register" />
 					</div>
 				</form>
 			</div>
---
+++ src/components/elements/ButtonPrimary.tsx
@@ -0,0 +1,14 @@
+interface ButtonProps {
+  label: string;
+  type: "submit" | "reset" | "button" | undefined;
+}
+
+const ButtonPrimary: React.FC<ButtonProps> = ({label, type}) => {
+	return (
+		<button
+			className="bg-gradient-to-r from-violet-800 to-violet-500 text-white rounded-xl px-10 py-2 text-sm font-semibold inline-flex items-center justify-center disabled:pointer-events-none disabled:opacity-50"
+			type={type}>{label}</button>
+	)
+}
+
+export default ButtonPrimary;
