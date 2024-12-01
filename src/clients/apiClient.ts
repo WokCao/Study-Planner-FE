4f94e8b7273c32669138ea054e34ec8ebcc0be37
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-12-02 00:44:39 +0700

    revert api url ternary

--- src/clients/apiClient.ts
+++ src/clients/apiClient.ts
@@ -1,5 +1,4 @@
-// const API_BASE_URL = import.meta.env.DEV ? import.meta.env.VITE_REACT_APP_API_LOCAL : import.meta.env.VITE_REACT_APP_API;
-const API_BASE_URL = import.meta.env.VITE_REACT_APP_API;
+const API_BASE_URL = import.meta.env.DEV ? import.meta.env.VITE_REACT_APP_API_LOCAL : import.meta.env.VITE_REACT_APP_API;
 
 export const fetcher = async (url: string, formData = {}, options = {}) => {
   const response = await fetch(`${API_BASE_URL}${url}`, {
