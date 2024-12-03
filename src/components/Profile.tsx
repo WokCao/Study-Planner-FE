0667514ba0b0ab6a88ade9f471ce526b0851e2d9
WokCao <henryquoccao@gmail.com>
2024-12-03 11:49:20 +0700

    Fix: remove unnecessary code

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,14 +2,12 @@ import Logout from './Logout';
 import useUserStore from '../hooks/useUserStore';
 
 function Profile() {
-  const username = useUserStore((state) => state.username);
   const email = useUserStore((state) => state.email);
 	const fullname = useUserStore((state) => state.fullname);
 	// const avatarUrl = useUserStore((state) => state.avatarUrl);
 
   return (
     <>
-      <h1 className='text-2xl'>Username: {username}</h1>
       <h1 className='text-2xl'>Email: {email}</h1>
 			<h1 className='text-2xl'>Full Name: {fullname}</h1>
       <Logout />
--- src/hooks/useUserStore.ts
+++ src/hooks/useUserStore.ts
@@ -2,11 +2,10 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 interface UserState {
-  username: string | null;
   email: string | null;
   fullname: string | null;
   avatarUrl: string | null;
-	setData: (data: { username: string, email: string, fullname: string, avatarUrl: string }) => void;
+	setData: (data: { email: string, fullname: string, avatarUrl: string }) => void;
   setFullname: (name: string) => void;
   setAvatar: (url: string) => void;
   clearData: () => void;
@@ -15,13 +14,11 @@ interface UserState {
 const useUserStore = create<UserState>()(
   persist(
     (set) => ({
-      username: null,
       email: null,
 			fullname: null,
 			avatarUrl: null,
 			setData: (data) =>
         set({
-					username: data.username,
 					email: data.email,
 					fullname: data.fullname,
 					avatarUrl: data.avatarUrl,
@@ -30,7 +27,6 @@ const useUserStore = create<UserState>()(
 			setAvatar: (avatarUrl: string) => set({ avatarUrl }),
       clearData: () =>
         set({
-					username: null,
 					email: null,
 					fullname: null,
 					avatarUrl: null,
