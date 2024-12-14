eaea33081c75f8009f4393cb920a56849d3f5e2c
21127561 <71368328+Royal-Mike@users.noreply.github.com>
2024-12-14 23:27:45 +0700

    update password
    update password

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -11,17 +11,44 @@ function Profile() {
 	const setFullname = useUserStore((state) => state.setFullname);
 	// const avatarUrl = useUserStore((state) => state.avatarUrl);
 
-	const mutation = useMutation({
+	const mutationFullname = useMutation({
     mutationFn: async (fname) => await fetcher('/users', {
       method: 'PUT',
 			body: JSON.stringify({ fullname: fname }),
       headers: { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' }
     }),
     onSuccess: (_data, variables) => {
+			Swal.fire({
+				title: "Success",
+				text: "Your full name has been changed.",
+				icon: "success"
+			});
       setFullname(String(variables));
     },
     onError: (error) => {
       console.error('Update failed:', error.message);
+    },
+  });
+
+	const mutationPassword = useMutation({
+    mutationFn: async ({oldPass, newPass} : {oldPass: string, newPass: string}) => await fetcher('/users', {
+      method: 'PUT',
+			body: JSON.stringify({ oldPassword: oldPass, password: newPass }),
+      headers: { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' }
+    }),
+    onSuccess: () => {
+      Swal.fire({
+				title: "Success",
+				text: "Your password has been changed.",
+				icon: "success"
+			});
+    },
+    onError: (error) => {
+      Swal.fire({
+				title: "Failure",
+				text: error.message,
+				icon: "error"
+			});
     },
   });
 
@@ -39,7 +66,54 @@ function Profile() {
 		});
 
 		if (fullname) {
-			mutation.mutate(fullname);
+			mutationFullname.mutate(fullname);
+		}
+	}
+
+	async function updatePassword() {
+		const { value: formValues } = await Swal.fire({
+			title: "Change Password",
+			html: `
+				<label class="swal2-input-label" for="swal-input1">Old Password</label>
+				<input id="swal-input1" class="swal2-input" type="password">
+				<label class="swal2-input-label" for="swal-input2">New Password</label>
+				<input id="swal-input2" class="swal2-input" type="password">
+				<label class="swal2-input-label" for="swal-input3">Confirm New Password</label>
+				<input id="swal-input3" class="swal2-input" type="password">
+			`,
+			focusConfirm: false,
+			showCancelButton: true,
+			preConfirm: () => {
+				return [
+					(document.getElementById("swal-input1") as HTMLInputElement).value,
+					(document.getElementById("swal-input2") as HTMLInputElement).value,
+					(document.getElementById("swal-input3") as HTMLInputElement).value
+				];
+			}
+		});
+
+		if (formValues) {
+			const oldPass = formValues[0];
+			const newPass = formValues[1];
+			const confirmNewPass = formValues[2];
+
+			if (!oldPass || !newPass || !confirmNewPass) {
+				return Swal.fire({
+					title: "Failure",
+					text: "Please enter all fields!",
+					icon: "error"
+				});
+			}
+
+			if (newPass !== confirmNewPass) {
+				return Swal.fire({
+					title: "Failure",
+					text: "Passwords do not match.",
+					icon: "error"
+				});
+			}
+
+			mutationPassword.mutate({ oldPass, newPass });
 		}
 	}
 
@@ -57,7 +131,7 @@ function Profile() {
 							</span>
 						</h1>
 						<h1 className='text-lg'>{email}</h1>
-						<button className='text-sm hover:underline mt-2'>
+						<button className='text-sm text-left hover:underline mt-2' onClick={updatePassword}>
 							Change Password
 						</button>
 					</div>
