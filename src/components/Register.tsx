ad48060cbaedd6dcfa6130d1dc536a5412338258
WokCao <henryquoccao@gmail.com>
2024-12-14 21:57:05 +0700

    Fix: change to register array

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -95,14 +95,14 @@ function Register() {
 					<FormInput
 						label='email'
 						type='email'
-						register={register('email', { required: 'Email is required' })}
+						register={[register('email', { required: 'Email is required' })]}
 						errors={errors}
 						error={errorEmail}
 					/>
 					<FormInput
 						label='full name'
 						type='text'
-						register={register('fullname', { required: 'Full name is required' })}
+						register={[register('fullname', { required: 'Full name is required' })]}
 						errors={errors}
 						error={errorFullname}
 						errorKey='fullname'
@@ -110,22 +110,21 @@ function Register() {
 					<FormInput
 						label='password'
 						type='password'
-						register={register('password', { required: 'Password is required' })}
+						register={[register('password', { required: 'Password is required' })]}
 						errors={errors}
 						error={errorPassword}
 					/>
 					<FormInput
 						label='confirm password'
 						type='password'
-						register={register('confirmPassword', {
+						register={[register('confirmPassword', {
 							required: 'Please confirm password',
 							validate: (value: string) => {
 								if (watch('password') !== value) {
 									return "Your passwords do not match";
 								}
 							}
-						})
-						}
+						})]}
 						errors={errors}
 						errorKey='confirmPassword'
 					/>
