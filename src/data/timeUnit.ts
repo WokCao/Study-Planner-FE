96dd5b3264ddfc7adc08ff136f449b4a7fb451b9
WokCao <henryquoccao@gmail.com>
2024-12-15 17:44:55 +0700

    Create: time unit converts to seconds

---
+++ src/data/timeUnit.ts
@@ -0,0 +1,9 @@
+const second = 1;
+const minute = 60;
+const hour = 60 * 60;
+const day = 60 * 60 * 24;
+const week = 60 * 60 * 24 * 7;
+const month = 60 * 60 * 24 * 30;
+const year = 60 * 60 * 24 * 30 * 12;
+
+export { second, minute, hour, day, week, month, year };
