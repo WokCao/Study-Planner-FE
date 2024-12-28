c7533f0b93e4a6c4e4562d577eb816295fd008ce
WokCao <henryquoccao@gmail.com>
2024-12-28 23:42:02 +0700

    Update: change text of AI button

--- src/components/elements/ButtonAI.tsx
+++ src/components/elements/ButtonAI.tsx
@@ -11,7 +11,7 @@ const ButtonAI = ({ AnalyzeSchedule }: IButtonAI) => {
             className="flex items-center bg-white w-fit px-2 py-2 group rounded-full shadow-lg text-xl hover:text-indigo-700 hover:cursor-pointer"
             onClick={AnalyzeSchedule}>
             <FontAwesomeIcon icon={faRobot} className="w-7 h-7" />
-            <span className="ms-2 group-hover:inline-block group-hover:animate-slide-in animate-slide-out pe-1">Get analysis of selected date from AI</span>
+            <span className="ms-2 group-hover:inline-block group-hover:animate-slide-in animate-slide-out pe-1">Get analysis of the interval from AI</span>
         </div>
     )
 }
