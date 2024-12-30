9c84262b90b6702eefc2f27423377792cdefe2f8
WokCao <henryquoccao@gmail.com>
2024-12-30 18:21:09 +0700

    Fix: format the value to show on dashboard

--- src/components/elements/Task.tsx
+++ src/components/elements/Task.tsx
@@ -2,8 +2,18 @@ import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Task from "../../interface/Task";
 import UpdateFormInterface from "../../interface/UpdateFrom";
+import { format } from "date-fns";
 
 const SingleTask = ({ task, setShowUpdateForm }: { task: Task, setShowUpdateForm: React.Dispatch<React.SetStateAction<UpdateFormInterface>> }) => {
+    let formattedDate = null;
+    try {
+        formattedDate = task.deadline
+            ? format(task.deadline, 'dd-MM-yyyy H:m').padEnd(16, '0')
+            : 'No deadline';
+    } catch (error: any) {
+        console.log(error)
+    }
+
     return (
         <div className="px-16 py-4 bg-white rounded-3xl flex items-center hover:cursor-pointer hover:shadow-xl" onClick={() => setShowUpdateForm({ isShown: true, task: task })}>
             <section className="flex flex-col w-10/12">
@@ -15,7 +25,7 @@ const SingleTask = ({ task, setShowUpdateForm }: { task: Task, setShowUpdateForm
                 </div>
             </section>
             <section className="ms-auto flex items-center">
-                <span className="mx-2 truncate">{task.deadline}</span>
+                <span className="mx-2 truncate">{formattedDate}</span>
                 <FontAwesomeIcon icon={faAngleRight} />
             </section>
         </div>
