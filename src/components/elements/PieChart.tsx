81cbb981803280ebc1c0aec0c7d1210883f1926b
WokCao <henryquoccao@gmail.com>
2025-01-06 01:14:04 +0700

    Update: margin of piechart

--- src/components/elements/PieChart.tsx
+++ src/components/elements/PieChart.tsx
@@ -15,7 +15,7 @@ function PieChart({ data }: IPieChart) {
     return (
         <ResponsivePie
             data={data}
-            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            margin={{ top: 40, right: 60, bottom: 0, left: 60 }}
             innerRadius={0.5}
             padAngle={0.7}
             cornerRadius={3}
